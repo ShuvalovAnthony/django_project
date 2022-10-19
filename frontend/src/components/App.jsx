8a7b858445277c74a572dc9d74d4364915e91ea7
ShuvalovAnthony <shuvalov.anthony@gmail.com>
2022-10-19 19:13:32 +0400

    fecth2

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,8 +3,6 @@ import { createSignal, createResource, createEffect, For } from "solid-js";
 async function fetchTopic(id) {
   return (await fetch(`http://127.0.0.1:8000/api/v1/topic/${id}`)).json();
 }
-
-
 async function fetchTopic2(id) {
   return (await fetch(`http://127.0.0.1:8000/api/v1/topic/${id}`)).json();
 }
