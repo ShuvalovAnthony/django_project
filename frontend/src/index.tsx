9c2bcd3a73ada10f354d2bb1835532967bc8fbcf
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2022-10-20 17:03:33 +0400

    update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,5 +4,5 @@ import { render } from 'solid-js/web';
 import App from './App';
 import FormApp from './Form'
 
-// render(() => <App />, document.getElementById('root') as HTMLElement);
+render(() => <App />, document.getElementById('root') as HTMLElement);
 render(() => <FormApp />, document.getElementById('form') as HTMLElement);
