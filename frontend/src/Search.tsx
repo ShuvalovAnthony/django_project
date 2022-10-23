ed022ef18cf66a75cb1fb913814e07975afd1a80
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2022-10-23 23:45:25 +0400

    interactive search

--- frontend/src/Search.tsx
+++ frontend/src/Search.tsx
@@ -1,74 +1,53 @@
 import { Component } from 'solid-js';
 import { useFormHandler, yupSchema } from 'solid-form-handler';
-import { Checkboxes } from './components/Checkboxes';
 import { TextInput } from './components/TextInput';
-import { Checkbox } from './components/Checkbox';
-import { Select } from './components/Select';
-import { Radios } from './components/Radios';
-
+import { createSignal, createResource, createEffect } from "solid-js";
 import * as yup from 'yup';
 
-type User = {
-    title: string;
-    content: string;
-    category: number;
+
+const fetchTopic = async (id: number) =>
+    (await fetch(`http://127.0.0.1:8000/api/v1/topic/${id}`)).json();
+
+type TopicId = {
+    topicid: number;
 };
 
-export const userSchema: yup.SchemaOf<User> = yup.object({
-    title: yup.string().required('Required field'),
-    content: yup.string().required('Required field'),
-    category: yup.number().required().required('Required field').typeError('Country is required'),
+export const userSchema: yup.SchemaOf<TopicId> = yup.object({
+    topicid: yup.number().required('Required field')
 });
 
 export const SearchApp: Component = () => {
     const formHandler = useFormHandler(yupSchema(userSchema));
     const { formData } = formHandler;
+    const [topicData, setTopicData] = createSignal(1);
+    const [topic] = createResource(topicData, fetchTopic);
 
-    const submit = async (event: Event) => {
-        event.preventDefault();
-        try {
-            await formHandler.validateForm();
-            fetch('http://127.0.0.1:8000/api/v1/topic/', {
-                method: 'POST',
-                headers: {
-                    'Accept-Encoding': 'gzip, deflate, br',
-                    'Content-Type': 'application/json',
-                    'Authorization': "Token b59184f08e80365dd1986d8c832187a866cd7217"
-                },
-                body: JSON.stringify(formData())
-            });
-            console.log('data was sent');
-            formHandler.resetForm();
-        } catch (error) {
-            console.error(error);
-        }
-    };
+    createEffect(async () => {
+        setTopicData(formData().topicid);
+    })
 
     return (
         <div class="container-fluid">
             <div class="container">
                 <div class="row">
                     <div class="col">
-                        <form onSubmit={submit}>
-                            <TextInput label="Title" name="title" formHandler={formHandler} />
-                            <TextInput label="Content" name="content" formHandler={formHandler} />
-                            <Select
-                                label="Category"
-                                name="category"
-                                placeholder="Select a category"
-                                options={[
-                                    { value: 1, label: 'Python' },
-                                    { value: 2, label: 'Ege' },
-                                    { value: 3, label: 'Electronics' },
-                                ]}
-                                formHandler={formHandler}
-                            />
-                            <button disabled={formHandler.isFormInvalid()}>Submit</button>
+                        <form>
+                            <TextInput label="Topic ID" name="topicid" formHandler={formHandler} />
                         </form>
+                    </div>
+                    <div class="col">
+                        {topic() && (
+                            <section>
+                                <h1>{topic().title}</h1>
+                                <p>{topic().category}</p>
+                            </section>
+                        )}
                     </div>
                 </div>
             </div>
         </div>
     );
 };
+
+
 export default SearchApp; 
